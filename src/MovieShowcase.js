9008802cc6154673b13b636d89502549a3b51b47
Sophie Gu <sophieqgu@gmail.com>
2021-03-11 01:44:46 +0000

    Automatically backed up by Learn

--- src/MovieShowcase.js
+++ src/MovieShowcase.js
@@ -7,7 +7,7 @@ export default class MovieShowcase extends Component {
   generateMovieCards = () => {
     // map over your movieData array and return an array of the correct JSX
     return movieData.map(movie =>
-      <MovieCard key = { movie.title } title = { movie.title } IMDBRating = { movie.IMDBRating } genres = { movie.genres } poster = { movie.poster } />
+      <MovieCard key={ movie.title } title={ movie.title } IMDBRating={ movie.IMDBRating } genres={ movie.genres } poster={ movie.poster } />
     )
   }
 
