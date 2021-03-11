1374c39a010d2aabb2fc68ebd68998af17807b7d
Sophie Gu <sophieqgu@gmail.com>
2021-03-11 01:41:19 +0000

    Automatically backed up by Learn

--- src/card-components/MovieCard.js
+++ src/card-components/MovieCard.js
@@ -33,7 +33,7 @@ export default class MovieCard extends Component {
     return (
       <div className="movie-card">
         {/* which component should receive which props? */}
-        <CardFront />
+        <CardFront poster={ this.props.poster } />
         <CardBack />
       </div>
     )
